649e00ce6ad4c3006bcdfb64b622c83c616fb10b
karol.j.frankowski@gmail.com <Kodpin3360>
2021-09-25 09:14:55 +0200

    tekst

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
-//        Użyć każdej z opcji
+//        Użyć każdej z
+        Scanner scan = new Scanner(System.in);
+        System.out.println("tekst1");
+        System.out.print("podaj liczbe: ");
+        int liczba = scan.nextInt();
 
+        System.out.println("twoja liczba to : " + liczba);
 
     }
 }
