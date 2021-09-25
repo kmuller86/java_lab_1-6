04925f193e11b92e90a187879d9b1edefa545290
karol.j.frankowski@gmail.com <Kodpin3360>
2021-09-25 09:55:35 +0200

    cw6

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,19 @@ public class Main {
 
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
+        
+        double[] tab5 = new double[5];
+        tab5[0] = 0.1;
+        tab5[1] = 0.2;
+        tab5[2] = 0.3;
+        tab5[3] = 0.4;
+        tab5[4] = 0.5;
 
+        System.out.println("dlugosc tablicy to: " + tab5.length + " a jej wartosci to:");
+        for(int i=0; i < tab5.length; i++){
+            System.out.print(tab5[i] + " ");
+        }
+
+        
     }
 }
